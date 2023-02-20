d450e061b1bf7d6bb9dd49c3b14124128948987a
root <root@DESKTOP-O1KC17T.localdomain>
2023-02-20 13:26:55 +0200

    add simple form to create a Lead

--- leads/urls.py
+++ leads/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name="leads"
 urlpatterns = [
-    path('', views.index)
+    path('', views.lead_list, name='leads'),
+    path('<int:pk>', views.lead_detail, name='lead_detail'),
+    path('create/', views.create, name='lead_create')
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.http import HttpResponse
-# Create your views here.
+from .models import Lead, Agent, User
+from .forms import LeadForm
+
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        'leads': leads
+    }
+    return render(request,'leads/lead_list.html', context)
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    print(lead)
+    context = {
+        'lead':lead
+    }
+    return render(request,'leads/lead_detail.html', context)
+
+def create(request):
+    form = LeadForm()
+    if request.method == 'POST':
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            first = form.cleaned_data['first_name']
+            last = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(first_name=first, last_name=last, age=age, agent=agent)
+
+            return redirect(reverse('leads:leads'))
+        
+    return render(request, 'leads/lead_create.html', {'form':form})
 
 
-def index(request):
-    return render(request,'index.html'  )
+
+
+
+
+
+
+
