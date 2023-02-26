10b7c549481ae815524dc573bb328a3fafb82059
root <root@DESKTOP-O1KC17T.localdomain>
2023-02-27 00:52:11 +0200

    Add enviroment veriables

--- core/settings.py
+++ core/settings.py
@@ -18,14 +18,27 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+ 
+import environ
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+
+READ_DOT_ENV_FILE = env.bool('READ_DOT_ENV_FILE', default=False)
+if READ_DOT_ENV_FILE:
+    environ.Env.read_env()
+    
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d&v+hlw3v2te288vk275(xx-wg679a8heh7)040lqrbfpu_=pp'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
+
+
 
 
 # Application definition
